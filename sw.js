97278c90bd5ea877058dcac94e98de1257de53fc
maximelami18 <maxime.lami18@gmail.com>
2026-08-27 03:48:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,31 @@
-self.addEventListener("install",function(e){self.skipWaiting();});self.addEventListener("activate",function(e){e.waitUntil(clients.claim());});self.addEventListener("fetch",function(e){e.respondWith(fetch(e.request));});
+var CACHE_NAME = 'btp-shell-v1';
+self.addEventListener('install', function(e){
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(['./', './index.html']);
+    })
+  );
+});
+self.addEventListener('activate', function(e){
+  e.waitUntil(Promise.all([
+    clients.claim(),
+    caches.keys().then(function(names){
+      return Promise.all(names.filter(function(n){return n!==CACHE_NAME;}).map(function(n){return caches.delete(n);}));
+    })
+  ]));
+});
+self.addEventListener('fetch', function(e){
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    fetch(e.request).then(function(res){
+      if(res&&res.ok&&e.request.url.indexOf(self.location.origin)===0){
+        var clone=res.clone();
+        caches.open(CACHE_NAME).then(function(cache){cache.put(e.request,clone);});
+      }
+      return res;
+    }).catch(function(){
+      return caches.match(e.request).then(function(cached){return cached||caches.match('./index.html');});
+    })
+  );
+});
